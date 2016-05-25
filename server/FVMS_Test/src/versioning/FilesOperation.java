0f7b8014211ebd1dd8d9be55a840feb852796b7f
Korrr <ccorina.ungur@gmail.com>
2016-05-25 15:49:22 +0300

    added test for removeFile

--- server/FVMS_Test/src/versioning/FilesOperation.java
+++ server/FVMS_Test/src/versioning/FilesOperation.java
@@ -1,5 +1,7 @@
 package versioning;
 
+import init.InitializationClass;
+
 import java.io.File;
 
 import org.junit.Assert;
@@ -22,46 +24,65 @@ public class FilesOperation {
 
 	@Before
 	public void setUp() {
+		InitializationClass.initialization();
 		fs = new FileSystem();
 		fsdb = FileSystemDB.getInstance();
 		standard_content = "some content".getBytes();
 		userEmail = "files@test.user";
-		UsersDB.getInstance().insertUser("filesTestUser", "parola",
-				userEmail);
+		UsersDB.getInstance().insertUser("filesTestUser", "parola", userEmail);
 		fileName = "testFile.txt";
 		message = "test message";
 		fsdb.removeAllFiles();
-		Tools.removeAllFiles(Settings.FS_ROOTFOLDER);
+		fs.removeFilesFromRoot();
+		fs.emptyTrash();
 	}
 
 	@Test
 	public void addNewFile_validInput() {
 		String result = fs.add_newFile(standard_content, fileName, userEmail);
 		Assert.assertEquals(Messages.File_added.toString(), result);
-		
+
 		String hash = Tools.hashContent(standard_content);
 		boolean isFile = fsdb.isFileInDB(Tools.hashContent(standard_content));
 		Assert.assertTrue(isFile);
-		
+
 		String path = fsdb.getFilePath(hash);
 		File file = new File(path);
 		Assert.assertTrue(file.exists());
-		
+
 		String folder_name = "" + fsdb.getOriginalFileID(hash);
 		Assert.assertTrue(path.contains("/" + folder_name + "/"));
 	}
-	
+
 	@Test
-	public void addChange_validInput(){
+	public void addChange_validInput() {
 		fs.add_newFile(standard_content, fileName, userEmail);
 		int id = fsdb.getChangeID(Tools.hashContent(standard_content));
 		byte[] changed_content = "changed content".getBytes();
-		String result = fs.addChange(id, userEmail, changed_content, fileName, message);
-		
+		String result = fs.addChange(id, userEmail, changed_content, fileName,
+				message);
+
 		Assert.assertEquals(Messages.File_added.toString(), result);
-		
+
 		String path = fsdb.getFilePath(Tools.hashContent(changed_content));
 		File file = new File(path);
 		Assert.assertTrue(file.exists());
+	}
+
+	@Test
+	public void removeFile_trashEnabled_validFile() {
+		fs.add_newFile(standard_content, fileName, userEmail);
+		int id = fsdb.getChangeID(Tools.hashContent(standard_content));
+		byte[] changed_content = "changed content".getBytes();
+		fs.addChange(id, userEmail, changed_content, fileName, "test change");
+
+		String result = fs.removeFile(id);
+		Assert.assertEquals(Messages.File_removed.toString(), result);
+
+		File folder = new File(Settings.FS_ROOTFOLDER + id);
+		Assert.assertFalse(folder.exists());
+
+		File trashFolder = new File(Settings.TRASH_FOLDER + id);
+		Assert.assertFalse(trashFolder.exists());
 	}
 }
