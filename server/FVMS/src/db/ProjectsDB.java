9061d64445a64e940759adbd2cd7c92d3c85d466
Korrr <ccorina.ungur@gmail.com>
2016-06-11 23:12:06 +0300

    refactored code

--- server/FVMS/src/db/ProjectsDB.java
+++ server/FVMS/src/db/ProjectsDB.java
@@ -126,39 +126,19 @@ public class ProjectsDB {
 
 	public List<Project> getProjects(int uid) {
 		List<Project> projects = new ArrayList<Project>();
-		String statement = "SELECT " + Columns.PPermissions_project + " FROM "
-				+ Tables.PROJECT_PERMISSIONS + " WHERE "
-				+ Columns.PPermissions_user + "=" + uid + " AND "
-				+ Columns.PPermissions_rights + " LIKE '%" + Permissions.READ
-				+ "%'";
-		ResultSet rs = db.executeStatement(statement);
-
+		ResultSet rs = getPermissionsRS(uid);
 		try {
 			while (rs.next()) {
 				int pid = rs.getInt(Columns.PPermissions_project.toString());
-				statement = "SELECT " + Columns.Projects_Name + " FROM "
-						+ Tables.PROJECTS + " WHERE " + Columns.Projects_ID
-						+ "=" + pid;
-				ResultSet nrs = db.executeStatement(statement);
-				nrs.first();
-				String name = nrs.getString(Columns.Projects_Name.toString());
-				statement = "SELECT " + Columns.ProjectFiles_RPath + ","
-						+ Columns.ProjectFiles_CID + " FROM "
-						+ Tables.PROJECT_FILES + " WHERE "
-						+ Columns.ProjectFiles_PID + "=" + pid;
-				ResultSet prs = db.executeStatement(statement);
+				String name = getProjectName(pid);
+				ResultSet prs = getFilesDetailsRS(pid);
 				ArrayList<File> files = new ArrayList<File>();
 				while (prs.next()) {
 					String path = prs.getString(Columns.ProjectFiles_RPath
 							.toString());
 					int currentFile_id = prs.getInt(Columns.ProjectFiles_CID
 							.toString());
-					statement = "SELECT " + Columns.Changes_date + ","
-							+ Columns.Changes_message + ","
-							+ Columns.Changes_owner + " FROM " + Tables.CHANGES
-							+ " WHERE " + Columns.Changes_ID + "="
-							+ currentFile_id;
-					ResultSet frs = db.executeStatement(statement);
+					ResultSet frs = getChangeDetailsRS(currentFile_id);
 					frs.first();
 					String lastModified = frs.getString(Columns.Changes_date
 							.toString());
@@ -166,20 +146,58 @@ public class ProjectsDB {
 							.toString());
 					String ownerName = frs.getString(Columns.Changes_owner
 							.toString());
-
 					files.add(new File(currentFile_id, pid, path, lastModified,
 							comment, ownerName));
 				}
-
 				projects.add(new Project(pid, name, files));
-
 			}
-
 		} catch (SQLException e) {
 			Logger.logERROR(e);
 		}
 		db.closeStatementsAndResultSets();
 		return projects;
+	}
+
+	private ResultSet getPermissionsRS(int uid) {
+		String statement = "SELECT " + Columns.PPermissions_project + " FROM "
+				+ Tables.PROJECT_PERMISSIONS + " WHERE "
+				+ Columns.PPermissions_user + "=" + uid + " AND "
+				+ Columns.PPermissions_rights + " LIKE '%" + Permissions.READ
+				+ "%'";
+		ResultSet rs = db.executeStatement(statement);
+		return rs;
+	}
+
+	private ResultSet getChangeDetailsRS(int currentFile_id) {
+		String statement;
+		statement = "SELECT " + Columns.Changes_date + ","
+				+ Columns.Changes_message + ","
+				+ Columns.Changes_owner + " FROM " + Tables.CHANGES
+				+ " WHERE " + Columns.Changes_ID + "="
+				+ currentFile_id;
+		ResultSet frs = db.executeStatement(statement);
+		return frs;
+	}
+
+	private ResultSet getFilesDetailsRS(int pid) {
+		String statement;
+		statement = "SELECT " + Columns.ProjectFiles_RPath + ","
+				+ Columns.ProjectFiles_CID + " FROM "
+				+ Tables.PROJECT_FILES + " WHERE "
+				+ Columns.ProjectFiles_PID + "=" + pid;
+		ResultSet prs = db.executeStatement(statement);
+		return prs;
+	}
+
+	private String getProjectName(int pid) throws SQLException {
+		String statement;
+		statement = "SELECT " + Columns.Projects_Name + " FROM "
+				+ Tables.PROJECTS + " WHERE " + Columns.Projects_ID
+				+ "=" + pid;
+		ResultSet nrs = db.executeStatement(statement);
+		nrs.first();
+		String name = nrs.getString(Columns.Projects_Name.toString());
+		return name;
 	}
 
 	public void clearProjects() {
